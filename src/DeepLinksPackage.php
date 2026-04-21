44381c846c94a1e9ca62d520664e7ae236a8cc6f
Derek Stephen McLean <delboy1978uk@gmail.com>
2026-04-21 16:12:06 +0200

    add package

--- src/DeepLinksPackage.php
+++ src/DeepLinksPackage.php
@@ -1,1 +1,24 @@
 <?php
+
+declare(strict_types=1);
+
+namespace Bone\DeepLinks;
+
+use Barnacle\Container;
+use Bone\Contracts\Container\ContainerInterface;
+use Bone\Contracts\Container\RegistrationInterface;
+use Bone\Router\Router;
+use Bone\Router\RouterConfigInterface;
+
+class DeepLinksPackage implements RegistrationInterface, RouterConfigInterface
+{
+    public function addToContainer(ContainerInterface $c): void
+    {
+
+    }
+
+    public function addRoutes(Container $c, Router $router)
+    {
+       $router->get('/.well-known', function () {return 'HELLO';} );
+    }
+}
